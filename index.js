ecfcabc5164581bd615bccaf81f01dd20a5d8a7a
Ankitshru <45864839+Ankitshru@users.noreply.github.com>
2023-03-11 16:32:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //const parents = document.querySelectorAll(".icon");
 const icons = document.querySelectorAll('.icon')
-const path = '/data.json';
+const path = 'responsive-result-summary-design/data.json';
 
 fetch(path)
 .then((response) => response.json())
